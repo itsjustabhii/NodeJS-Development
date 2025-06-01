ed0bab5befa5a13d12110dc7c9c80dd85ff084f2
Abhishek Harish <itsjustabhii@gmail.com>
2025-06-01 14:10:57 +0530

    Http CreateServer

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const http = require("http")
 
-const Server = http.createServer()
+const server = http.createServer(function (req,res) {
+
+    if(req.url === "/getSecretData"){
+        res.end("There is no secret data")
+    }
+    res.end("Hello World")
+}) // returns an server instance
+
+server.listen(6666)
