e18afb01c4f1633e4481c863037bf95bfc8697b3
Abhishek Harish <itsjustabhii@gmail.com>
2025-06-07 17:05:54 +0530

    Connecting to Database server

--- database.js
+++ database.js
@@ -1,4 +1,21 @@
-require("mongodb")
-const URI = "mongodb+srv://itsjustabhii:Lewishamilton@nodetutorial.odvzbzm.mongodb.net/"
+const {MongoClient} = require("mongodb")
+const url = "mongodb+srv://itsjustabhii:Lewishamilton@nodetutorial.odvzbzm.mongodb.net/"
 
+const client = new MongoClient(url)
+
+const dbName = 'NodeTest'
+
+async function main() { //connect to client
+    await client.connect()
+    console.log('Connected successfully to Server')
+    const db = client.db(dbName)
+    const collection = db.collection('User')
+
+    return 'Done.'
+}
+
+main()
+.then(console.log)
+.catch(console.error)
+.finally(()=> client.close())
 
